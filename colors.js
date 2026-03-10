9d03efe89d8ad0571066388b4f15ba09e226a6a7
shin-kado <sh.kado.1254@gmail.com>
2026-03-10 10:49:26 +0900

    Add web app files and ignore private data

---
+++ colors.js
@@ -0,0 +1,24 @@
+// 【カラーパレット設定ファイル】
+// 0番目は常に Native Balance (ETH/BNB) に割り当てられます。
+const COLOR_PALETTE = [
+    '#00c805', // 0: Vivid Green (Native)
+    '#36a2eb', // 1: Electric Blue
+    '#ff9f40', // 2: Bright Orange
+    '#9966ff', // 3: Royal Purple
+    '#4bc0c0', // 4: Cyan
+    '#ff6384', // 5: Soft Pink
+    '#ffcd56', // 6: Sunny Yellow
+    '#45aaf2', // 7: High Blue
+    '#a55eea', // 8: Lavender
+    '#fd9644', // 9: Orange Tag
+    '#2bcbba', // 10: Teal
+    '#eb3b5a', // 11: Crimson
+    '#fa8231', // 12: Pumpkin
+    '#8854d0', // 13: Gloomy Purple
+    '#2d98da', // 14: Sky Blue
+    '#20bf6b', // 15: Jade Green
+    '#f7b731', // 16: Gold
+    '#4b7bec', // 17: Cinerama Blue
+    '#a5b1c2', // 18: Silver
+    '#778ca3'  // 19: Steel Grey
+];
