17408708ffa4644d2a1b31c3aa55e3bf359b284d
shin-kado <sh.kado.1254@gmail.com>
2026-03-11 07:40:26 +0900

    update

---
+++ web-app/contracts/SimpleStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 public storedData;
+
+    // 数字を保存する関数
+    function set(uint256 x) public {
+        storedData = x;
+    }
+
+    // テストです保存された数字を見る関数（storedDataがpublicなので自動で作られますが、明示的にも書けます）
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
