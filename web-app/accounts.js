83367c091a22b62099d0a699b00330b9dbe17132
shin-kado <sh.kado.1254@gmail.com>
2026-03-10 13:05:29 +0900

    Fix: Add accounts.js from correct path and update ignore

---
+++ web-app/accounts.js
@@ -0,0 +1,7 @@
+// accounts.js
+const MASTER_ACCOUNT_LIST = [
+    { name: "Acc1", address: "0x0799F37F871E0280f3149Ca7e4868BEef06f749a" },
+    { name: "Acc2", address: "0xe8365C42215FfDBc84f6AF48CB3DC513EBfe3ba1" },
+    { name: "Acc3", address: "0xD224f78a97A2e04455b9b32047F40e92B0f3768b" }
+    // 今後、ここに { name: "Friend", address: "0x..." } と追加するだけでOK！
+];
