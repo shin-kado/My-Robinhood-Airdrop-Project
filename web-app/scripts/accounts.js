31c349fe86d12b474ddcb38df4b2feb1b194f58e
shin-kado <sh.kado.1254@gmail.com>
2026-03-21 17:27:17 +0900

    UPDATE

--- web-app/scripts/accounts.js
+++ web-app/scripts/accounts.js
@@ -1,14 +1,17 @@
 const MASTER_ACCOUNT_LIST = [
     {
+        "name": "Main Wallet",
+        "address": "0x1234567890abcdef1234567890abcdef12345678",
+        "order": 10
+    },
+    {
+        "name": "Secondary Wallet",
+        "address": "0x2222222222222222222222222222222222222222",
+        "order": 20
+    },
+    {
         "name": "Demo Wallet A",
-        "address": "0x11111111111111111111"
-    },
-    {
-        "name": "Demo Wallet B",
-        "address": "0x22222222222222222222"
-    },
-    {
-        "name": "Demo Wallet C",
-        "address": "0x33333333333333333333"
+        "address": "0x1111111111111111111111111111111111111111",
+        "order": 30
     }
 ];
